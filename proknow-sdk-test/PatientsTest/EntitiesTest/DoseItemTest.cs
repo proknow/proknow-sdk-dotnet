4885049ac600f9f3a7b6873d36363898364fcd7b
Terry Wolf <Terry.Wolf@elekta.com>
2020-02-03 13:19:46 -0600

    Add DoseItem.Download()

---
+++ proknow-sdk-test/PatientsTest/EntitiesTest/DoseItemTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ProKnow.Test;
+
+namespace ProKnow.Patients.Entities.Test
+{
+    [TestClass]
+    public class DoseItemTest
+    {
+        [TestMethod]
+        public async Task DownloadAsyncTest()
+        {
+            var proKnow = new ProKnow(TestSettings.BaseUrl, TestSettings.CredentialsFile);
+            var workspace = await proKnow.Workspaces.FindAsync(t => t.Name == TestSettings.TestWorkspaceName);
+            var patientSummary = await proKnow.Patients.FindAsync(workspace.Id, t => t.Name == TestSettings.TestPatientName);
+            var patientItem = await patientSummary.GetAsync();
+            var entitySummary = patientItem.FindEntities(t => t.Type == "dose").First();
+            var doseItem = await entitySummary.GetAsync();
+            string folder = Path.Combine(Path.GetTempPath(), "DoseItemTest_DownloadAsyncTest");
+            string file = await doseItem.Download(folder);
+            Directory.Delete(folder, true);
+        }
+    }
+}
