de4c51d8c0592472eb469f6987fb67f2c0896fcd
Terry Wolf <Terry.Wolf@elekta.com>
2020-03-10 07:56:56 -0500

    Finish ScorecardTemplates tests

--- proknow-sdk-test/ScorecardTest/ScorecardTemplatesTest.cs
+++ proknow-sdk-test/ScorecardTest/ScorecardTemplatesTest.cs
@@ -16,6 +16,8 @@ namespace ProKnow.Scorecard.Test
         private static string _baseName = "SDK-ScorecardTemplatesTest";
         private static ComputedMetric _computedMetric;
         private static CustomMetricItem _customMetricItem;
+        private static List<ComputedMetric> _computedMetrics;
+        private static List<string> _customMetricNames;
         private static ScorecardTemplateItem _scorecardTemplateItem;
 
         [TestInitialize]
@@ -31,11 +33,9 @@ namespace ProKnow.Scorecard.Test
             _customMetricItem = await _proKnow.CustomMetrics.CreateAsync(_baseName, "patient", "number");
 
             // Create scorecard template for testing
-            var computedMetrics = new List<ComputedMetric>() { _computedMetric };
-            var customMetricNames = new List<string>() { _customMetricItem.Name };
-
-            //TODO--THE CUSTOM METRICS COME BACK AS IDS
-            _scorecardTemplateItem = await _proKnow.ScorecardTemplates.CreateAsync(_baseName, computedMetrics, customMetricNames);
+            _computedMetrics = new List<ComputedMetric>() { _computedMetric };
+            _customMetricNames = new List<string>() { _customMetricItem.Name };
+            _scorecardTemplateItem = await _proKnow.ScorecardTemplates.CreateAsync(_baseName, _computedMetrics, _customMetricNames);
         }
 
         [TestCleanup]
@@ -60,6 +60,84 @@ namespace ProKnow.Scorecard.Test
             Assert.AreEqual(1, _scorecardTemplateItem.CustomMetrics.Count);
             var customMetricItem = _scorecardTemplateItem.CustomMetrics[0];
             Assert.AreEqual(_customMetricItem.Id, customMetricItem.Id);
+        }
+
+        [TestMethod]
+        public async Task DeleteAsyncTest()
+        {
+            // Delete the scorecard template created in initialization
+            await _proKnow.ScorecardTemplates.DeleteAsync(_scorecardTemplateItem.Id);
+
+            // Verify the scorecard template was deleted
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.FindAsync(t => t.Name == _baseName);
+            Assert.IsNull(scorecardTemplate);
+
+            // Restore the deleted scorecard template
+            _scorecardTemplateItem = await _proKnow.ScorecardTemplates.CreateAsync(_baseName, _computedMetrics, _customMetricNames);
+        }
+
+        [TestMethod]
+        public async Task FindAsyncTest()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.FindAsync(t => t.Name == _baseName);
+            Assert.AreEqual(_scorecardTemplateItem.Id, scorecardTemplate.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
+        }
+
+        [TestMethod]
+        public async Task GetAsyncTest()
+        {
+            var scorecardTemplateItem = await _proKnow.ScorecardTemplates.GetAsync(_scorecardTemplateItem.Id);
+            Assert.AreEqual(_baseName, scorecardTemplateItem.Name);
+            Assert.AreEqual(1, scorecardTemplateItem.ComputedMetrics.Count);
+            var computedMetric = scorecardTemplateItem.ComputedMetrics[0];
+            Assert.AreEqual(_computedMetric.Type, computedMetric.Type);
+            Assert.AreEqual(_computedMetric.RoiName, computedMetric.RoiName);
+            Assert.AreEqual(_computedMetric.Arg1, computedMetric.Arg1);
+            Assert.AreEqual(_computedMetric.Arg2, computedMetric.Arg2);
+            Assert.AreEqual(1, scorecardTemplateItem.CustomMetrics.Count);
+            var customMetricItem = scorecardTemplateItem.CustomMetrics[0];
+            Assert.AreEqual(_customMetricItem.Id, customMetricItem.Id);
+        }
+
+        [TestMethod]
+        public async Task ResolveAsyncTest_Id()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.ResolveAsync(_scorecardTemplateItem.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Id, scorecardTemplate.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
+        }
+
+        [TestMethod]
+        public async Task ResolveAsyncTest_Name()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.ResolveAsync(_scorecardTemplateItem.Name);
+            Assert.AreEqual(_scorecardTemplateItem.Id, scorecardTemplate.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
+        }
+
+        [TestMethod]
+        public async Task ResolveByIdAsyncTest()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.ResolveByIdAsync(_scorecardTemplateItem.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Id, scorecardTemplate.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
+        }
+
+        [TestMethod]
+        public async Task ResolveByNameAsyncTest()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.ResolveByNameAsync(_scorecardTemplateItem.Name);
+            Assert.AreEqual(_scorecardTemplateItem.Id, scorecardTemplate.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
+        }
+
+        [TestMethod]
+        public async Task QueryAsyncTest()
+        {
+            var scorecardTemplates = await _proKnow.ScorecardTemplates.QueryAsync();
+            var scorecardTemplate = scorecardTemplates.First(t => t.Id == _scorecardTemplateItem.Id);
+            Assert.AreEqual(_scorecardTemplateItem.Name, scorecardTemplate.Name);
         }
     }
 }
