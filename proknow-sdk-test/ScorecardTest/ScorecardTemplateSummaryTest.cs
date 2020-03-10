3cdff108c4a34bf21dbd77c56fe1e43bdaf9fa33
Terry Wolf <Terry.Wolf@elekta.com>
2020-03-10 08:02:37 -0500

    Add ScorecardTemplateSummary tests

---
+++ proknow-sdk-test/ScorecardTest/ScorecardTemplateSummaryTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ProKnow.CustomMetric;
+using ProKnow.Test;
+
+namespace ProKnow.Scorecard.Test
+{
+    [TestClass]
+    public class ScorecardTemplateSummaryTest
+    {
+        private static ProKnow _proKnow = TestSettings.ProKnow;
+        private static string _baseName = "SDK-ScorecardTemplatesTest";
+        private static ComputedMetric _computedMetric;
+        private static CustomMetricItem _customMetricItem;
+        private static List<ComputedMetric> _computedMetrics;
+        private static List<string> _customMetricNames;
+        private static ScorecardTemplateItem _scorecardTemplateItem;
+
+        [TestInitialize]
+        public async Task ClassInitialize()
+        {
+            // Cleanup from previous test failure, if necessary
+            await ClassCleanup();
+
+            // Create computed metric for testing
+            _computedMetric = new ComputedMetric("VOLUME_PERCENT_DOSE_RANGE_ROI", "BODY", 0, 0.05);
+
+            // Create custom metric for testing
+            _customMetricItem = await _proKnow.CustomMetrics.CreateAsync(_baseName, "patient", "number");
+
+            // Create scorecard template for testing
+            _computedMetrics = new List<ComputedMetric>() { _computedMetric };
+            _customMetricNames = new List<string>() { _customMetricItem.Name };
+            _scorecardTemplateItem = await _proKnow.ScorecardTemplates.CreateAsync(_baseName, _computedMetrics, _customMetricNames);
+        }
+
+        [TestCleanup]
+        public async Task ClassCleanup()
+        {
+            // Delete scorecard template and custom metric created for this test
+            await TestHelper.DeleteScorecardTemplatesAsync(_baseName);
+            await TestHelper.DeleteCustomMetricsAsync(_baseName);
+        }
+
+        [TestMethod]
+        public async Task GetAsyncTest()
+        {
+            var scorecardTemplate = await _proKnow.ScorecardTemplates.FindAsync(t => t.Id == _scorecardTemplateItem.Id);
+            var scorecardTemplateItem = await scorecardTemplate.GetAsync();
+            Assert.AreEqual(_baseName, scorecardTemplateItem.Name);
+            Assert.AreEqual(1, scorecardTemplateItem.ComputedMetrics.Count);
+            var computedMetric = scorecardTemplateItem.ComputedMetrics[0];
+            Assert.AreEqual(_computedMetric.Type, computedMetric.Type);
+            Assert.AreEqual(_computedMetric.RoiName, computedMetric.RoiName);
+            Assert.AreEqual(_computedMetric.Arg1, computedMetric.Arg1);
+            Assert.AreEqual(_computedMetric.Arg2, computedMetric.Arg2);
+            Assert.AreEqual(1, scorecardTemplateItem.CustomMetrics.Count);
+            var customMetricItem = scorecardTemplateItem.CustomMetrics[0];
+            Assert.AreEqual(_customMetricItem.Id, customMetricItem.Id);
+        }
+    }
+}
