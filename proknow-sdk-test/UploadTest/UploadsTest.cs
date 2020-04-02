258bf97767bdf20045038b05d6847ba87218b0ab
Terry Wolf <Terry.Wolf@elekta.com>
2020-04-02 09:56:05 -0500

    Finish Uploads tests

--- proknow-sdk-test/UploadTest/UploadsTest.cs
+++ proknow-sdk-test/UploadTest/UploadsTest.cs
@@ -2,6 +2,7 @@
 using ProKnow.Patient;
 using ProKnow.Patient.Entities;
 using ProKnow.Test;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -10,57 +11,52 @@ namespace ProKnow.Upload.Test
     [TestClass]
     public class UploadsTest
     {
-        private static string _patientMrnAndName = "SDK-UploadsTest";
+        private static string _testClassName = "SDK-UploadsTest";
         private static ProKnow _proKnow = TestSettings.ProKnow;
         private static Uploads _uploads = _proKnow.Uploads;
-        private static string _workspaceId;
-        private static PatientSummary _patientSummary;
 
         [ClassInitialize]
         public static async Task ClassInitialize(TestContext testContext)
         {
-            // Delete test workspace, if necessary
-            await TestHelper.DeleteWorkspaceAsync(_patientMrnAndName);
-
-            // Create a test workspace
-            var workspaceItem = await TestHelper.CreateWorkspaceAsync(_patientMrnAndName);
-            _workspaceId = workspaceItem.Id;
-
-            // Create a test patient
-            _patientSummary = await TestHelper.CreatePatientAsync(_patientMrnAndName);
+            // Delete test workspaces, if necessary
+            await TestHelper.DeleteWorkspacesAsync(_testClassName);
         }
 
         [ClassCleanup]
         public static async Task ClassCleanup()
         {
-            // Delete test workspace
-            await _proKnow.Workspaces.DeleteAsync(_workspaceId);
+            // Delete test workspaces
+            await TestHelper.DeleteWorkspacesAsync(_testClassName);
         }
 
         [TestMethod]
-        public async Task UploadAsyncTest_SingleFile()
+        public async Task UploadAsyncTest_FilePath()
         {
+            int testNumber = 1;
+
+            // Create a workspace
+            var workspaceItem = await TestHelper.CreateWorkspaceAsync(_testClassName, testNumber);
+
+            // Create a patient
+            var patientItem = await TestHelper.CreatePatientAsync(_testClassName, testNumber);
+
             // Upload test file
             var uploadPath = Path.Combine(TestSettings.TestDataRootDirectory, "Becker^Matthew", "RP.dcm");
             var overrides = new UploadFileOverrides
             {
-                Patient = new PatientCreateSchema { Name = _patientMrnAndName, Mrn = _patientMrnAndName }
+                Patient = new PatientCreateSchema { Mrn = patientItem.Mrn, Name = patientItem.Name }
             };
-            var uploadBatch = await _uploads.UploadAsync(_workspaceId, uploadPath, overrides);
+            var uploadBatch = await _uploads.UploadAsync(workspaceItem.Id, uploadPath, overrides);
 
             // Wait until uploaded test file has processed
-            PatientItem patientItem;
             while (true)
             {
-                patientItem = await _patientSummary.GetAsync();
-                var entitySummaries = patientItem.FindEntities(t => t.Type == "plan");
-                if (entitySummaries.Count > 0 && entitySummaries[0].Status == "completed")
+                await patientItem.RefreshAsync();
+                var entitySummaries = patientItem.FindEntities(t => true);
+                if (entitySummaries.Count == 1 && entitySummaries[0].Status == "completed")
                 {
                     // Make sure the uploaded data is the same
                     Assert.AreEqual(entitySummaries[0].Uid, "2.16.840.1.114337.1.1.1535997926.0");
-
-                    // Cleanup (in case there are other tests using the same patient)
-                    await entitySummaries[0].DeleteAsync();
 
                     break;
                 }
@@ -68,37 +64,77 @@ namespace ProKnow.Upload.Test
         }
 
         [TestMethod]
-        public async Task UploadAsyncTest_SingleFolder()
+        public async Task UploadAsyncTest_FolderPath()
         {
+            int testNumber = 2;
+
+            // Create a workspace
+            var workspaceItem = await TestHelper.CreateWorkspaceAsync(_testClassName, testNumber);
+
+            // Create a patient
+            var patientItem = await TestHelper.CreatePatientAsync(_testClassName, testNumber);
+
             // Upload test folder
             var uploadPath = Path.Combine(TestSettings.TestDataRootDirectory, "Becker^Matthew", "CT");
             var overrides = new UploadFileOverrides
             {
-                Patient = new PatientCreateSchema { Name = _patientMrnAndName, Mrn = _patientMrnAndName }
+                Patient = new PatientCreateSchema { Mrn = patientItem.Mrn, Name = patientItem.Name }
             };
-            var uploadBatch = await _uploads.UploadAsync(_workspaceId, uploadPath, overrides);
-            var uploadedFiles = Directory.GetFiles(uploadPath);
+            var uploadBatch = await _uploads.UploadAsync(workspaceItem.Id, uploadPath, overrides);
 
-            // Wait until uploaded test file has processed
-            PatientItem patientItem;
+            // Wait until uploaded test files have processed
             while (true)
             {
-                patientItem = await _patientSummary.GetAsync();
-                var entitySummaries = patientItem.FindEntities(t => t.Type == "image_set");
-                if (entitySummaries.Count > 0 && entitySummaries[0].Status == "completed")
+                await patientItem.RefreshAsync();
+                var entitySummaries = patientItem.FindEntities(t => true);
+                if (entitySummaries.Count == 1 && entitySummaries[0].Status == "completed")
                 {
                     // Make sure the uploaded data is the same
                     var entityItem = await entitySummaries[0].GetAsync() as ImageSetItem;
-                    if (entityItem.Data.Images.Count == uploadedFiles.Length)
-
-                        // Cleanup (in case there are other tests using the same image set)
-                        await entityItem.DeleteAsync();
-
+                    if (entityItem.Data.Images.Count == 5)
+                    {
                         break;
+                    }
                 }
             }
         }
 
-        //todo--UploadAsyncTest_MultipleFilesAndOrFolders
+        [TestMethod]
+        public async Task UploadAsyncTest_MultiplePaths()
+        {
+            int testNumber = 3;
+
+            // Create a workspace
+            var workspaceItem = await TestHelper.CreateWorkspaceAsync(_testClassName, testNumber);
+
+            // Create a patient without data
+            var patientItem = await TestHelper.CreatePatientAsync(_testClassName, testNumber);
+
+            // Upload test folder and test file
+            var uploadPath1 = Path.Combine(TestSettings.TestDataRootDirectory, "Becker^Matthew", "CT");
+            var uploadPath2 = Path.Combine(TestSettings.TestDataRootDirectory, "Becker^Matthew", "RS.dcm");
+            var uploadPaths = new List<string>() { uploadPath1, uploadPath2 };
+            var overrides = new UploadFileOverrides
+            {
+                Patient = new PatientCreateSchema { Mrn = patientItem.Mrn, Name = patientItem.Name }
+            };
+            var uploadBatch = await _uploads.UploadAsync(workspaceItem.Id, uploadPaths, overrides);
+
+            // Verify test folder and test file were uploaded (may have to wait for files to be processed)
+            while (true)
+            {
+                await patientItem.RefreshAsync();
+                var entitySummaries = patientItem.FindEntities(t => true);
+                if (entitySummaries.Count == 2 && entitySummaries[0].Status == "completed" && entitySummaries[1].Status == "completed")
+                {
+                    var imageSetSummary = patientItem.FindEntities(t => t.Type == "image_set")[0];
+                    var entityItem = await imageSetSummary.GetAsync() as ImageSetItem;
+                    if (entityItem.Data.Images.Count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
