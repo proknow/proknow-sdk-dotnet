edea6215e807959e204f3c58f2d453943623bb21
Terry Wolf <Terry.Wolf@elekta.com>
2020-04-28 14:17:17 -0500

    Add FrameOfReferenceUid property to EntityItem

--- proknow-sdk/Patient/Entities/EntityItem.cs
+++ proknow-sdk/Patient/Entities/EntityItem.cs
@@ -31,6 +31,12 @@ namespace ProKnow.Patient.Entities
         public string PatientId { get; set; }
 
         /// <summary>
+        /// The entity frame of reference UID
+        /// </summary>
+        [JsonPropertyName("frame_of_reference")]
+        public string FrameOfReferenceUid { get; set; }
+
+        /// <summary>
         /// The entity ProKnow ID
         /// </summary>
         [JsonPropertyName("id")]
