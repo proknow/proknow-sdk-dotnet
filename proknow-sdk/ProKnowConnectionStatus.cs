c64a508855e7a21b80281094d79cf614c73517c1
Terry Wolf <Terry.Wolf@elekta.com>
2020-04-28 14:19:45 -0500

    Remove unnecessary usings

--- proknow-sdk/ProKnowConnectionStatus.cs
+++ proknow-sdk/ProKnowConnectionStatus.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ProKnow
+﻿namespace ProKnow
 {
     /// <summary>
     /// Provides the status of the ProKnow API connection
