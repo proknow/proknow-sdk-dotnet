304e7991b1311e725694b60f528e572a77967be5
thiphuongthaophung <99750042+thiphuongthaophung@users.noreply.github.com>
2022-07-01 13:10:52 -0500

    [stories/US323157] Restart stopped DICOM Agent SCP services (#31)
    * Added status code to response for getCredentials route
    
    * Added new release details
    
    * Updated readme for deployment notes
    
    * Updated comments

---
+++ .github/pull_request_template.md
@@ -0,0 +1,4 @@
+## Before requesting a reviewer, complete this checklist.
+
+- [ ] I have updated `releaseHistory.md`.
+- [ ] I have updated the `<Version>` attribute with the new version in `proknow-sdk.csproj` following the [semantic versioning](https://semver.org/) rules.
--- proknow-sdk/ProKnowCredentialsStatus.cs
+++ proknow-sdk/ProKnowCredentialsStatus.cs
@@ -1,4 +1,6 @@
-﻿namespace ProKnow
+﻿using System.Net;
+
+namespace ProKnow
 {
     /// <summary>
     /// Provides the ProKnow credentials (API key) status
@@ -17,15 +19,22 @@
         public string ErrorMessage { get; private set; }
 
         /// <summary>
+        ///  The<see cref="HttpStatusCode"/> string value
+        /// </summary>
+        public string ErrorCode { get; private set; }
+
+        /// <summary>
         /// Creates a ProKnowCredentialsStatus object
         /// </summary>
         /// <param name="isValid">The flag indicating whether the specified ProKnow credentials (API key) is valid
         /// for the ProKnow subdomain specified by the base URL</param>
         /// <param name="errorMessage">The error message or null if the credentials (API key) is invalid</param>
-        internal ProKnowCredentialsStatus(bool isValid, string errorMessage = null)
+        /// <param name="errorCode">The <see cref="HttpStatusCode"/> string value</param>
+        internal ProKnowCredentialsStatus(bool isValid, string errorMessage = null, string errorCode = null)
         {
             IsValid = isValid;
             ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
         }
     }
 }
