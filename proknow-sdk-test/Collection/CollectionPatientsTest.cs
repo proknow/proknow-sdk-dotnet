4d892dda9481e856d86192680d32236b61d99a10
lanyadasilva <97463070+lanyadasilva@users.noreply.github.com>
2022-11-23 14:38:14 -0600

    [stories/US331823] .NET SDK returns only 200 patients (#36)
    * paging for patients and collection patients queries
    
    * updates from PR
    
    * requested PR changes

--- proknow-sdk-test/Collection/CollectionPatientsTest.cs
+++ proknow-sdk-test/Collection/CollectionPatientsTest.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProKnow.Patient.Entities;
+using ProKnow.Patient;
 using ProKnow.Test;
 using System.Collections.Generic;
 using System.IO;
@@ -112,6 +114,49 @@ namespace ProKnow.Collection.Test
             Assert.AreEqual(1, collectionPatientSummaries.Count);
             Assert.IsTrue(collectionPatientSummaries.Any(p => p.Workspace.Id == workspaceItem2.Id &&
                 p.Patient.Id == patientSummary2.Id && p.Entity.Id == entitySummary2.Id));
+        }
+
+        [TestMethod]
+        public async Task QueryAsyncTest_Paging()
+        {
+            var testNumber = 3;
+
+            // Create a test workspace
+            var workspaceItem = await TestHelper.CreateWorkspaceAsync(_testClassName, testNumber);
+
+            // Create a test collection
+            var collectionItem = await _proKnow.Collections.CreateAsync($"SDK-{_testClassName}-{testNumber}-Name",
+                $"SDK-{_testClassName}-{testNumber}-Description", "workspace", new List<string>() { workspaceItem.Id });
+
+            // Create enough test patients to invoke paging (over 200)
+            var numPatients = 210;
+            var patientItems = await TestHelper.CreateMultiPatientAsync(_testClassName, testNumber, numPatients);
+
+            var patientCollectionTasks = new List<Task>();
+            for (var i = 0; i < numPatients; i++)
+            {
+                // Add the patient and structure set to the collection
+                var savedPatientItems = new List<CollectionPatientsAddSchema>() { new CollectionPatientsAddSchema(patientItems[i].Id) };
+                patientCollectionTasks.Add(collectionItem.Patients.AddAsync(workspaceItem.Id, savedPatientItems));
+            }
+            await Task.WhenAll(patientCollectionTasks);
+
+            // Query the items in the collection
+            var collectionPatientSummaries = await collectionItem.Patients.QueryAsync();
+
+            // Sort by ID for comparison
+            var patientItemsList = new List<PatientItem>(patientItems);
+            var collectionPatientSummariesList = new List<CollectionPatientSummary>(collectionPatientSummaries);
+            patientItemsList.Sort((x, y) => x.Id.CompareTo(y.Id));
+            collectionPatientSummariesList.Sort((x, y) => x.Patient.Id.CompareTo(y.Patient.Id));
+
+            // Verify the items returned
+            Assert.AreEqual(numPatients, collectionPatientSummaries.Count);
+            for (var i = 0; i < numPatients; i++)
+            {
+                Assert.AreEqual(patientItemsList[i].Id, collectionPatientSummariesList[i].Patient.Id);
+                Assert.AreEqual(patientItemsList[i].Name, collectionPatientSummariesList[i].Patient.Name);
+            }
         }
     }
 }
